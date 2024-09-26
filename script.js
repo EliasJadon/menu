fb242d5fcc9eb6c69ac9fb1a2dd333200f0777a2
EliasJadon <elias.jadon1@gmail.com>
2024-09-27 01:33:43 +0300

    add images

--- script.js
+++ script.js
@@ -1,19 +1,59 @@
 const menu = {
     starters: [
-        { name: "סלט קיסר", description: "חסה טרייה, קרוטונים ורוטב קיסר" },
-        { name: "ברוסקטה", description: "לחם קלוי עם עגבניות ובזיליקום" }
+        { 
+            name: "סלט קיסר", 
+            description: "חסה טרייה, קרוטונים ורוטב קיסר", 
+            price: "₪35", 
+            image: "images/salad.jpg" 
+        },
+        { 
+            name: "ברוסקטה", 
+            description: "לחם קלוי עם עגבניות ובזיליקום", 
+            price: "₪28", 
+            image: "images/drink.jpg" 
+        }
     ],
     main: [
-        { name: "עוף בגריל", description: "מוגש עם ירקות ופירה" },
-        { name: "ספגטי קרבונרה", description: "פסטה ברוטב שמנת ופנצ'טה" }
+        { 
+            name: "עוף בגריל", 
+            description: "מוגש עם ירקות ופירה", 
+            price: "₪70", 
+            image: "images/drink.jpg" 
+        },
+        { 
+            name: "ספגטי קרבונרה", 
+            description: "פסטה ברוטב שמנת ופנצ'טה", 
+            price: "₪65", 
+            image: "images/salad.jpg" 
+        }
     ],
     desserts: [
-        { name: "עוגת גבינה", description: "עוגת גבינה עשירה עם רוטב פירות יער" },
-        { name: "טירמיסו", description: "קינוח איטלקי עם קפה ומסקרפונה" }
+        { 
+            name: "עוגת גבינה", 
+            description: "עוגת גבינה עשירה עם רוטב פירות יער", 
+            price: "₪40", 
+            image: "images/salad.jpg" 
+        },
+        { 
+            name: "טירמיסו", 
+            description: "קינוח איטלקי עם קפה ומסקרפונה", 
+            price: "₪38", 
+            image: "images/drink.jpg" 
+        }
     ],
     drinks: [
-        { name: "קפוצ'ינו", description: "אספרסו עשיר עם חלב מוקצף" },
-        { name: "לימונדה", description: "לימונים סחוטים טריים עם נענע" }
+        { 
+            name: "קפוצ'ינו", 
+            description: "אספרסו עשיר עם חלב מוקצף", 
+            price: "₪15", 
+            image: "images/drink.jpg" 
+        },
+        { 
+            name: "לימונדה", 
+            description: "לימונים סחוטים טריים עם נענע", 
+            price: "₪12", 
+            image: "images/salad.jpg" 
+        }
     ]
 };
 
@@ -24,7 +64,14 @@ function filterMenu(type) {
     menu[type].forEach(item => {
         const menuItem = document.createElement("div");
         menuItem.className = "menu-item";
-        menuItem.innerHTML = `<h3>${item.name}</h3><p>${item.description}</p>`;
+        menuItem.innerHTML = `
+            <img src="${item.image}" alt="${item.name}">
+            <div>
+                <h3>${item.name}</h3>
+                <p>${item.description}</p>
+                <p class="price">${item.price}</p>
+            </div>
+        `;
         menuContainer.appendChild(menuItem);
     });
 }
